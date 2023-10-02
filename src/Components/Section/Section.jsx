0dcff18c356b8acb1d6221525ff088c4304769f9
surabhi <surabhibhusari2@gmail.com>
2023-10-02 17:09:06 +0530

    add

--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -1,13 +1,18 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
-import style from '../Navbar/Navbar.module.css'
+import style from "./Section.module.css"
 import Image from "../../assets/im.png";
 
 function  Section(){
     return(
-        <div className={style.wrapper}>
-            <p className={style.p}>You don't have to <b>Fight them Alone</b></p>
-            
-     <img src={Image} className={style.img} alt='Image' size={30}/>
+        <div>
+            <div>
+            <p className={style.p}>You don't have to
+             <b>Fight them Alone</b></p>
+             </div>
+            <div className={style.wrapper}>
+     <img src={Image} className={style.img} alt='Image' size={20}/>
+     </div>
      </div>
     )
 };
